cabe3294e48d4e3fc04ede6dbf13324345bb4863
GabrielPanta <gabrielpantajimenez@hotmail.com>
2019-05-13 17:49:36 -0500

    GuardarDB en localStorage

--- JavaScript/animacion.js
+++ JavaScript/animacion.js
@@ -1,3 +1,26 @@
-function saludo(){
-    alert("hola");
+//variablesGlobales
+const formularioUI=document.querySelector('#formulario');
+const listaActividadesUI=document.getElementById('listaActividades');
+let arrayActividades=[];
+
+//Funciones
+const CrearItem=(actividad)=>{
+    let item={
+        actividad:actividad,
+        estado:'false'
+    }
+    arrayActividades.push(item);
+    return item;
 }
+const GuardarDB=()=>{
+    localStorage.setItem('rutina',JSON.stringify(arrayActividades));
+}
+
+//EventListener
+formularioUI.addEventListener('submit',(e)=>{
+e.preventDefault();
+let actividadUI=document.querySelector('#actividad').value;
+CrearItem(actividadUI);
+GuardarDB();
+formularioUI.reset(); 
+});
