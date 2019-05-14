2860ab16c2964d86c896271b43b2f97ecc089ee4
GabrielPanta <gabrielpantajimenez@hotmail.com>
2019-05-14 16:05:54 -0500

    Completo(Totaly)
    https://www.youtube.com/watch?v=zeb5PsxWijY&list=LL25NCzSS4fdLWn9GmOMyNtg&index=4&t=3009s

--- JavaScript/animacion.js
+++ JavaScript/animacion.js
@@ -1,26 +1,63 @@
 //variablesGlobales
-const formularioUI=document.querySelector('#formulario');
-const listaActividadesUI=document.getElementById('listaActividades');
-let arrayActividades=[];
+const formularioUI = document.querySelector('#formulario');
+const listaActividadesUI = document.getElementById('listaActividades');
+let arrayActividades = [];
 
 //Funciones
-const CrearItem=(actividad)=>{
-    let item={
-        actividad:actividad,
-        estado:'false'
+const CrearItem = (actividad) => {
+    let item = {
+        actividad: actividad,
+        estado: 'false'
     }
     arrayActividades.push(item);
     return item;
 }
-const GuardarDB=()=>{
-    localStorage.setItem('rutina',JSON.stringify(arrayActividades));
+const GuardarDB = () => {
+    localStorage.setItem('rutina', JSON.stringify(arrayActividades));
+    PintarDB();
+}
+const PintarDB = () => {
+    listaActividadesUI.innerHTML = "";
+    arrayActividades = JSON.parse(localStorage.getItem('rutina'));
+    if (arrayActividades === null) {
+        arrayActividades = [];
+    } else {
+        arrayActividades.forEach(element => {
+            listaActividadesUI.innerHTML += `<div class="alert alert-danger" role="alert"> <i class="material-icons float-left mr-2">
+accessibility </i> <b>${element.actividad}</b> - ${element.estado} <span class="float-right"><i class="material-icons">
+done</i> <i class="material-icons"> delete </i> </span></div>`
+        });
+    }
 }
 
+const EliminarDB = (actividad) => {
+    console.log(actividad);
+}
+
+
+
 //EventListener
-formularioUI.addEventListener('submit',(e)=>{
-e.preventDefault();
-let actividadUI=document.querySelector('#actividad').value;
-CrearItem(actividadUI);
-GuardarDB();
-formularioUI.reset(); 
+formularioUI.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let actividadUI = document.querySelector('#actividad').value;
+    CrearItem(actividadUI);
+    GuardarDB();
+    formularioUI.reset();
 });
+
+document.addEventListener('DOMContentLoaded', PintarDB);
+listaActividadesUI.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    console.log(e.target.innerHTML);
+
+    if (e.target.innerHTML === 'done' || e.target.innerHTML === 'delete') {
+        let texto = e.path[2].childNodes[3].innerHTML;
+        if (e.target.innerHTML === 'delete') {
+            EliminarDB(texto);
+        }
+        if (e.target.innerHTML === 'done') {
+
+        }
+    }
+})
---
+++ JavaScript/app.js
@@ -0,0 +1,74 @@
+//Variables Globales
+const formularioUI = document.querySelector('#formulario');
+const listaActividadesUI = document.getElementById('listaActividades');
+let arrayActividades = [];
+
+//Funciones
+const CrearItem = (actividad) => {
+    let item = {
+        actividad: actividad,
+        estado: 'false'
+    }
+    arrayActividades.push(item);
+    return item; 
+}
+
+const GuardarDB = () => {
+    localStorage.setItem('rutina', JSON.stringify(arrayActividades));
+     PintarDB();
+}
+const PintarDB = () => {
+    listaActividadesUI.innerHTML = "";
+    arrayActividades = JSON.parse(localStorage.getItem('rutina'));
+    if (arrayActividades === null) {
+        arrayActividades = [];
+    } else {
+        arrayActividades.forEach(element => {
+            if(element.estado===true){
+                listaActividadesUI.innerHTML += `<div class="alert alert-success" role="alert"> <i class="material-icons float-left mr-2">accessibility </i> <b>${element.actividad}</b> - ${element.estado} <span class="float-right"><i class="material-icons">done</i> <i class="material-icons">delete</i> </span></div>`
+            }else{
+                listaActividadesUI.innerHTML += `<div class="alert alert-danger" role="alert"> <i class="material-icons float-left mr-2">accessibility </i> <b>${element.actividad}</b> - ${element.estado} <span class="float-right"><i class="material-icons">done</i> <i class="material-icons">delete</i> </span></div>`
+            }
+            
+        });
+    }
+}
+const EliminarDB = (actividad) => {
+    let indexArray;
+arrayActividades.forEach((element,index)=>{
+    if(element.actividad===actividad){
+        indexArray=index;
+    }
+});
+    arrayActividades.splice(indexArray,1);
+    GuardarDB();
+}
+const EditarDB=(actividad)=>{
+    let indexArray=arrayActividades.findIndex((element)=>{
+        return element.actividad===actividad});
+    arrayActividades[indexArray].estado=true;
+    GuardarDB();
+}
+
+//Event Listener
+formularioUI.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let actividadUI = document.querySelector('#actividad').value;
+    CrearItem(actividadUI);
+    GuardarDB();
+    formularioUI.reset();
+});
+document.addEventListener('DOMContentLoaded', PintarDB);
+listaActividadesUI.addEventListener('click',(e)=>{
+    e.preventDefault();
+    
+    if(e.target.innerHTML==='done'||e.target.innerHTML==='delete'){
+        let texto=(e.path[2].childNodes[3].innerHTML);
+        if(e.target.innerHTML==='delete'){
+            EliminarDB(texto);
+        }
+        if(e.target.innerHTML==='done'){
+            EditarDB(texto);
+        }
+    }
+}); 
